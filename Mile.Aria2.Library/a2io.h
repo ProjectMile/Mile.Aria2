345e7eb9072490897a21cac976217ab5154b9612
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-04-04 23:10:19 +0800

    Make 64-Bit a2_off_t to fix the maximum 4GB file limitation.

--- Mile.Aria2.Library/a2io.h
+++ Mile.Aria2.Library/a2io.h
@@ -141,7 +141,11 @@
 #  define a2open(path, flags, mode) _wsopen(path, flags, _SH_DENYNO, mode)
 #  define a2fopen(path, mode) _wfsopen(path, mode, _SH_DENYNO)
 // # define a2ftruncate(fd, length): We don't use ftruncate in Mingw build
+#ifdef _MSC_VER
+#  define a2_off_t int64_t
+#else
 #  define a2_off_t off_t
+#endif // _MSC_VER
 #elif defined(__ANDROID__) || defined(ANDROID)
 #  define a2lseek(fd, offset, origin) lseek64(fd, offset, origin)
 // # define a2fseek(fp, offset, origin): No fseek64 and not used in aria2
