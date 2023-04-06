c0a255015e0066db86152bed8d7420fb5b357fd8
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-04-06 22:34:18 +0800

    Add release packaging support for BuildAllTargets.cmd.

--- BuildAllTargets.cmd
+++ BuildAllTargets.cmd
@@ -13,4 +13,17 @@ call "%~dp0Mile.Project.Windows\InitializeVisualStudioEnvironment.cmd"
 rem Build all targets
 MSBuild -binaryLogger:Output\BuildAllTargets.binlog -m BuildAllTargets.proj
 
+rem Extract files from NuGet package to folder
+7z x Output\Mile.Aria2.nupkg "-o.\Output\Mile.Aria2"
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+
+rem Copy Documents
+copy .\Output\Mile.Aria2\*.md .\Output\Mile.Aria2\Redist\
+rename .\Output\Mile.Aria2\Redist\*.md *.txt
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+
+rem Build SDK Package
+7z a -r Output\Mile.Aria2.zip .\Output\Mile.Aria2\Redist\*.*
+
+
 @endlocal
