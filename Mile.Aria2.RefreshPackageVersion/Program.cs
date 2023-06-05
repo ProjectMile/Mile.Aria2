6ea4cef79f12322db71e2d4c0cbfdd6ebf3cf4d0
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-06-05 23:32:45 +0800

    Add Mile.Aria2.RefreshPackageVersion project.

---
+++ Mile.Aria2.RefreshPackageVersion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mile.Aria2.RefreshPackageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
