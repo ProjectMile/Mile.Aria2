e44c2285a6ee27d957afcb9340421e79b72f0b8c
MouriNaruto <Kenji.Mouri@outlook.com>
2024-03-15 19:46:09 +0800

    Add Mile.Aria2.WinRT project.

---
+++ Mile.Aria2.WinRT/Mile.Aria2.WinRT.cpp
@@ -0,0 +1,25 @@
+﻿/*
+ * PROJECT:   Mile.Aria2
+ * FILE:      Mile.Aria2.WinRT.cpp
+ * PURPOSE:   Implementation for Mile.Aria2.WinRT
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <clocale>
+#include <cstdio>
+#include <cwchar>
+
+int main()
+{
+    std::wprintf(
+        L"Mile.Aria2.WinRT\n"
+        L"================================================================\n"
+        L"Hello World!\n");
+
+    std::getchar();
+
+    return 0;
+}
