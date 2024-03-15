83f2a5893630fba3931474891368cde05cf643af
MouriNaruto <Kenji.Mouri@outlook.com>
2024-03-16 00:01:18 +0800

    Start to implement Mile.Aria2.SessionManager.

---
+++ Mile.Aria2.WinRT/SessionManager.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "SessionManager.h"
+#if __has_include("SessionManager.g.cpp")
+#include "SessionManager.g.cpp"
+#endif
+
+namespace winrt::Mile::Aria2::implementation
+{
+    int32_t SessionManager::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void SessionManager::MyProperty(int32_t /*value*/)
+    {
+        throw hresult_not_implemented();
+    }
+}
